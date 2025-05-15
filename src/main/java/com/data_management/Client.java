60fd115ed8e22eea3dfd54e0c012001d54b61c73
Adrien <a.deldelaguila@student.maastrichtuniversity.nl>
2025-05-15 21:03:59 +0200

    client

--- src/main/java/com/data_management/Client.java
+++ src/main/java/com/data_management/Client.java
@@ -1,10 +1,7 @@
 package com.data_management;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 
-import org.java_websocket.WebSocket;
-import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.handshake.ServerHandshake;
 import org.java_websocket.client.WebSocketClient;
 
